81fc036d25dd6c4ec9fefda55c8e920bdd9f61be
Bùi Mạnh <22022602@vnu.edu.vn>
2025-05-04 12:36:47 +0700

    Thêm route để xử lí ảnh động trong production

---
+++ frontend/app/api/assets/users/[filename]/route.ts
@@ -0,0 +1,19 @@
+import { NextRequest, NextResponse } from 'next/server';
+import path from 'path';
+import fs from 'fs';
+
+export async function GET(req: NextRequest, { params }: { params: { filename: string } }) {
+  const filePath = path.join(process.cwd(), 'assets', 'users', params.filename);
+
+  if (fs.existsSync(filePath)) {
+    const ext = path.extname(filePath).slice(1);
+    const fileBuffer = fs.readFileSync(filePath);
+    return new NextResponse(fileBuffer, {
+      headers: {
+        'Content-Type': `image/${ext}`,
+      },
+    });
+  } else {
+    return NextResponse.json({ error: 'File not found' }, { status: 404 });
+  }
+}
